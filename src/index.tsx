8959eb18d0fbbecf56189194fa92e2f14eb3b0ad
severjason <ssever2010@gmail.com>
2018-04-23 00:41:43 +0300

    added provider and store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { App } from './containers/';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, Store } from 'redux';
+import { ChpState } from './interfaces';
+import rootReducer from './reducers';
+import Routes from './routes';
+
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const store: Store<ChpState> = createStore(rootReducer);
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
