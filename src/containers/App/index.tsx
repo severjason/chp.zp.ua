f8e3f7f2d81e5718c01d39c1cdeed9bb191f74e2
severjason <ssever2010@gmail.com>
2018-04-23 15:11:22 +0300

    Added ImageDivider

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
-import {  Footer, Header, /*ImageDivider*/ Partners, Services } from '../../components';
+import {  Footer, Header, ImageDivider, Partners, Services } from '../../components';
 import { ChpState } from '../../interfaces';
 import './index.css';
 
-class App extends React.Component {
+class App extends React.Component<any> {
     public render() {
         return (
             <div className="app-wrapper">
                 <Header {...this.props}/>
                 <Services/>
-{/*
-                <ImageDivider/>
-*/}
+                <ImageDivider {...this.props.images[1]}/>
                 <Services/>
                 <Partners/>
                 <Footer/>
