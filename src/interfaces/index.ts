44868e78cf21339f45d732917bd92ddb42ff8e81
severjason <ssever2010@gmail.com>
2018-04-25 00:59:24 +0300

    New interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,9 +1,23 @@
 export interface ChpImage {
+    class?: string;
     right: boolean;
-    src: any;
+    src: string;
     text: string;
+}
+
+export interface ChpServicesAndGoods {
+    title: string;
+    texts: string[];
+}
+
+export interface ChpPartner {
+    title: string;
+    logo: string;
 }
 
 export interface ChpState {
     images: ChpImage[];
+    services: ChpServicesAndGoods;
+    goods: ChpServicesAndGoods;
+    partners: ChpPartner[];
 }
