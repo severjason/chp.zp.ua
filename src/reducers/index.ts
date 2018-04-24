beec030e941ae75d30eb5e01d15e1dab1b21129f
severjason <ssever2010@gmail.com>
2018-04-25 00:59:13 +0300

    Initial state update

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,4 +1,9 @@
 import * as airConditioningImg from '../img/img2.jpg';
+import * as greeLogo from '../img/logos/gree.png';
+import * as kentatsuLogo from '../img/logos/kentatsu.png';
+import * as mitsubishiLogo from '../img/logos/mitsubishi.png';
+import * as sakataLogo from '../img/logos/sakata.jpg';
+
 import { ChpState } from '../interfaces';
 
 const INITIAL_STATE: ChpState = {
@@ -7,18 +12,61 @@ const INITIAL_STATE: ChpState = {
             right: true,
             src: airConditioningImg,
             text: `
-            Beat the Heat During the Extremely
-            Hot Summer!
+            Насладись Приятной Прохладой в Течение Жаркого Лета!
             `,
         },
         {
+            class: 'chp-image-mountain',
             right: false,
             src: airConditioningImg,
             text: `
-            Beat the Heat During the Extremely
-            Hot Summer2!
+            Почувствуй Преимущество Комфортной Температуры в Вашем Доме!
             `,
-        }
+        },
+        {
+            class: 'chp-image-maintenance',
+            right: true,
+            src: airConditioningImg,
+            text: `
+            Качественные Решения Вопросов Кондиционирования Воздуха
+            `,
+        },
+    ],
+    services: {
+        title: 'Услуги',
+        texts: [
+            'Монтаж, обслуживание и ремонт кондиционеров, ' +
+            'систем кондиционирования и вентиляции (в том числе чистка наружных и внутренних блоков, ' +
+            'дозаправка фреоном и т.п.)',
+            'Проектирование систем кондиционирования и вентиляции',
+            'Ремонт холодильного и торгово-технического оборудования',
+        ]
+    },
+    goods: {
+        title: 'Товары',
+        texts: [
+            'Бытовые кондиционеры Mitsubishi Electric, Gree, Kentatsu, Sakata',
+            'Системы кондиционирования и вентиляции Mitsubishi Electric, Gree, Kentatsu, Sakata',
+            'Холодильные и морозильные камеры "под ключ" (до 300 куб.м.);',
+        ]
+    },
+    partners: [
+        {
+            title: 'Gree',
+            logo: greeLogo,
+        },
+        {
+            title: 'Kentatsu',
+            logo: kentatsuLogo,
+        },
+        {
+            title: 'Mitsubishi Electric',
+            logo: mitsubishiLogo,
+        },
+        {
+            title: 'Sakata',
+            logo: sakataLogo,
+        },
     ]
 };
 
