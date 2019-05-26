f2d41c5846eabea69e49be1feace3c6e3f46e84d
severjason <ssever2010@gmail.com>
2019-05-26 15:27:30 +0300

    config changes

--- src/index.tsx
+++ src/index.tsx
@@ -8,21 +8,26 @@ import rootReducer from './reducers';
 import Routes from './routes';
 import { ThemeProvider } from 'styled-components';
 import { mainTheme, GlobalStyle } from 'styles';
-
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const store: Store<ChpState> = createStore(rootReducer);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <ThemeProvider theme={mainTheme}>
-                <GlobalStyle>
-                    <Routes/>
-                </GlobalStyle>
-            </ThemeProvider>
-        </BrowserRouter>
-    </Provider>,
+    (
+        <Provider store={store}>
+            <BrowserRouter>
+                <ThemeProvider theme={mainTheme}>
+                    <GlobalStyle>
+                        <Routes/>
+                    </GlobalStyle>
+                </ThemeProvider>
+            </BrowserRouter>
+        </Provider>
+    ),
     document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
